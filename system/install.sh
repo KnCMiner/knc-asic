c89b9bd0544fc2d9aa3d680a38fab22b710ffa5c
Henrik Nordstrom <henrik@henriknordstrom.net>
2014-07-08 12:32:06 +0000

    install.sh: Install waas in /usr/bin

--- system/install.sh
+++ system/install.sh
@@ -10,5 +10,7 @@ ln -sf $PWD/../rc.local /etc/
 sudo ln -sf $PWD/dhclient-lcd.sh /etc/dhcp/dhclient-enter-hooks.d/
 sudo ln -sf $PWD/../asic /usr/bin/
 sudo ln -sf $PWD/../../spi-test/spi-test /usr/bin/
+sudo ln -sf $PWD/../waas/waas /usr/bin/
+sudo mkdir -p /config
 #cp rc-local.service /etc/systemd/system/
 #systemctl enable rc-local
