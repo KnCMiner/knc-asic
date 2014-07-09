0192ec4980c3a8e3208cb064ff6282f2d135b08f
Henrik Nordstrom <henrik@henriknordstrom.net>
2014-07-09 22:53:37 +0000

    lcd: Switch to ifupdown for getting IP notifications

--- system/install.sh
+++ system/install.sh
@@ -7,10 +7,14 @@ echo "optargs=capemgr.disable_partno=BB-BONELT-HDMI,BB-BONELT-HDMIN" >>/boot/uEn
 umount /boot
 cp BB-* /lib/firmware/
 ln -sf $PWD/../rc.local /etc/
-sudo ln -sf $PWD/dhclient-lcd.sh /etc/dhcp/dhclient-enter-hooks.d/
+sudo ln -sf $PWD/lcd-ifupdown.sh /etc/network/if-up.d/lcd
+sudo ln -sf $PWD/lcd-ifupdown.sh /etc/network/if-post-down.d/lcd
+sudo ln -sf $PWD/../lcd-message /usr/bin/
 sudo ln -sf $PWD/../asic /usr/bin/
 sudo ln -sf $PWD/../../spi-test/spi-test /usr/bin/
 sudo ln -sf $PWD/../waas/waas /usr/bin/
+sudo ln -sf $PWD/../knc-serial /usr/bin
+sudo ln -sf $PWD/../knc-led /usr/bin
 sudo mkdir -p /config
 #cp rc-local.service /etc/systemd/system/
 #systemctl enable rc-local
